bf9418f70a1bac5964a771938647f6f140e04e8e
siyabulela <siyabulelamadlaka@gmail.com>
2023-03-20 01:22:08 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,5 @@ printf("size of int: %lu bytes(s)\n", (unsigned long)sizeof(b));
 printf("size of long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 printf("size of long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
 printf("size of float: %lu bytes(s)\n", (unsigned long)sizeof(f));
-Return (0);
+return (0);
 }
