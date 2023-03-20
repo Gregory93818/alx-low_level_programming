21907ff1893a563a6081987a201defff10f936ea
siyabulela <siyabulelamadlaka@gmail.com>
2023-03-20 05:14:23 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - main function
  *
- * Return: always 0
+ * Return: 0 (Success)
  */
 
 int main(void)
@@ -13,5 +13,6 @@ int main(void)
 	printf("size of a long int: %lu byte(s)", sizeof(long int));
 	printf("size of a long long int: %lu byte(s)", sizeof(long long int));
 	printf("size of float: %lu byte(s)", sizeof(float));
+
 	return (0);
 }
